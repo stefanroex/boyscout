0bb9cef84e1d04b8bbde5d709b77451cf71aecba
Stefan Roex <stefanroex@gmail.com>
2019-11-25 17:29:17 +0100

    Add tests

--- src/bs/board.cljs
+++ src/bs/board.cljs
@@ -28,40 +28,81 @@
                  [0 1] #{}
                  [1 1] #{}
                  [2 1] #{[2 0]}}}
-  ```")
+  ```"
+  (:require [clojure.set :as set]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Board graph
 
-(defn- in-board? [width height [x y]])
-(defn- adjacent-coords [width height [x y]])
+(defn- in-board? [width height [x y]]
+  (and (< -1 x width)
+       (< -1 y height)))
+
+(defn- adjacent-coords [width height [x y]]
+  (set/select #(in-board? width height %)
+              #{[(inc x) y]
+                [(dec x) y]
+                [x (inc y)]
+                [x (dec y)]}))
+
+(defn- make-edges [width height]
+  (into {} (for [x (range width)
+                 y (range height)
+                 :let [coord [x y]]]
+             [coord (adjacent-coords width height coord)])))
 
 (defn make [width height]
-  ;; Steps needed and pseudo code:
-  ;; 1. Generate all coordinates in a board of width and height
-  ;;    tips:
-  ;;      - (range width) will give you a range between 0 and width-1.
-  ;;      - For every width (range width), generate a tuple of [width h]
-  ;;        for every height in (range height)
-  ;; 2. For every point, what are it's neighbors?
-  ;; 3. Filter the neighbors that are outside of the board
-  ;; 4. Build the edges as a map from every coordinate to a set of it's neighbors.
-  )
+  {:board/width width
+   :board/height height
+   :board/source nil
+   :board/target nil
+   :board/walls #{}
+   :board/edges (make-edges width height)})
 
-(defn all-coordinates [board])
-(defn neighbor-coords [board pos])
+(defn all-coordinates [board]
+  (keys (:board/edges board)))
+
+(defn neighbor-coords [board pos]
+  (get-in board [:board/edges pos]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Queries
 
-(defn source? [board pos])
-(defn target? [board pos])
-(defn wall? [board pos])
+(defn source? [board pos]
+  (= (:board/source board) pos))
+
+(defn target? [board pos]
+  (= (:board/target board) pos))
+
+(defn wall? [board pos]
+  (= (get-in board [:board/edges pos]) #{}))
+
+(def connected? (complement wall?))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mutations
 
-(defn set-source [board pos])
-(defn set-target [board pos])
-(defn make-wall [board pos])
-(defn destroy-wall [board pos])
+(defn set-source [board pos]
+  (assoc board :board/source pos))
+
+(defn set-target [board pos]
+  (assoc board :board/target pos))
+
+;; walls
+
+(defn- update-edge [board p1 p2 f]
+  (-> board
+      (update-in [:board/edges p1] f p2)
+      (update-in [:board/edges p2] f p1)))
+
+(defn- connected-neighbor-coords [{:board/keys [width height] :as board} pos]
+  (set/select #(connected? board %) (adjacent-coords width height pos)))
+
+(defn- update-walls [board pos f]
+  (reduce #(update-edge %1 %2 pos f) board (connected-neighbor-coords board pos)))
+
+(defn make-wall [board pos]
+  (update-walls board pos disj))
+
+(defn destroy-wall [board pos]
+  (update-walls board pos conj))
